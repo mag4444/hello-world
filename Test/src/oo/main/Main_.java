5c98b22bb2967466d962f8707d0e880956793c7a
mag4444 <mag4444@gmail.com>
2017-05-12 11:19:13 +0900

    test

--- Test/src/oo/main/Main_.java
+++ Test/src/oo/main/Main_.java
@@ -2,7 +2,6 @@ package oo.main;
 
 public class Main_ {
 	public static void main(String[] args) {
-		System.out.println("»Â»Â»Â");
-		System.out.println("hohoho");
+		System.out.println("chchchchchch");
 	}
 }
